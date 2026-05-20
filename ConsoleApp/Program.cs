32ce4a9670a0ea46f732cd9a146424195d8f6b79
zenin5479 <zenin5479@gmail.com>
2026-05-20 15:51:47 +0300

    Update 2.8 Обновлен метод. Проведен рефакторинг

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using SharedStorage;
+using System;
 
 namespace ConsoleApp
 {
--- WinFormsApp/MainForm.cs
+++ WinFormsApp/MainForm.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using SharedStorage;
+using System;
 using System.Windows.Forms;
-using SharedStorage;
 
 namespace WinFormsApp
 {
